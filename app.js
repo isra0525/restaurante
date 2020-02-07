3e3a9e8088b76c9af3ae00fcdfb30e7f93dbd55e
Israel Candia <candia.israel@gmail.com>
2020-02-07 10:43:28 -0500

    rutas

--- app.js
+++ app.js
@@ -3,34 +3,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/restaurante',{ useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://admin:123456@127.0.0.1:27017/restaurante',{ useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> console.log("Mongo DB Connected"))
 .catch(err=> {
     console.log(Error, err.message);
 })
 
-//mongoose.connect('mongodb://user:password')
-const mysql = require('mysql');
-
-const db = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodemysql'
-})
-
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Mysql Db connected');
-})
-
-global.db = db;
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoriasRouter = require('./routes/categorias');
+var platosRouter = require('./routes/platos');
+var clientesRouter = require('./routes/clientes');
 
 var app = express();
 
@@ -43,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/categorias', categoriasRouter);
+app.use('/platos', platosRouter);
+app.use('/clientes', clientesRouter);
+
+
 
 
 module.exports = app;
